d5c14c29c32705a34007350ff5f3ded3ec756390
Mark Notton <mark@marknotton.uk>
2016-10-11 00:38:21 +0100

    no message

--- svg/twigextensions/sprite.php
+++ svg/twigextensions/sprite.php
@@ -24,10 +24,9 @@ class sprite extends \Twig_Extension {
     }
 
     $symbol  = is_string(func_get_arg(0)) ? func_get_arg(0) : false;
-    $fallback = true;
-    $useFallbackImage = true;
-    $size = true;
-    $browser = false;
+    $fallback = false;
+    $setSize = true;
+    $browserCriteria = false;
     $imageDir = craft()->svg->directory('imagesDirectory','images','/assets/images');
     $spritesDir = craft()->svg->directory('spritesDirectory','sprites','/assets/images/sprites');
     $symbolExists = file_exists(getcwd().($spritesDir.$symbol.'.svg'));
@@ -41,22 +40,24 @@ class sprite extends \Twig_Extension {
       } else if ( craft()->svg->isSize($settings) === true || $settings === true ) {
         // SIZE
         // If settings contains one or two numbers only, assume you want to define the size
-        $size = $settings;
-      } else if (is_string($settings) || $settings === false ) {
+        $setSize = $settings;
+      } else if (is_string($settings)) {
         // BROWSER SETTINGS
         $fallback = true;
-        $browser = $settings;
+        $browserCriteria = $settings;
+      } else if ($settings === false) {
+        // NO FALLBACK
+        $fallback = false;
       }
     }
 
-    // Only do a Browser check is symbol exists and browser is defined
-    $unsupportedBrowsers = $symbolExists && $browser ? craft()->svg->browserCheck($browser) : false;
-
     // Define a fallback image
-    if ($symbolExists !== false || $fallback !== false && $unsupportedBrowsers === true ) {
+    if ($symbolExists && $fallback !== false && is_string($browserCriteria) && craft()->svg->browserCheck($browserCriteria) ) {
       // Check the fallback is a string, otherwise use the symbol string as the image reference
-      $fallback = is_string($fallback) ? $fallback : $symbol;
-      $fallback = craft()->svg->fallbackImage($fallback, $imageDir);
+      $fallbackString = is_string($fallback) ? $fallback : $symbol;
+      $fallback = craft()->svg->fallbackImage($fallbackString, $imageDir);
+    } else {
+      $fallback = false;
     }
 
     // Classes
@@ -69,37 +70,40 @@ class sprite extends \Twig_Extension {
     // Size
     $dimensions = '';
 
-
-    if ($size !== false) {
-      $size = is_string($size) ? explode(' ', $size) : $size;
+    if ($setSize !== false ) {
+      $size = is_string($setSize) ? explode(' ', $setSize) : $setSize;
 
       if (is_array($size)) {
         $width = $size[0];
         $height = count($size) == 1 ? $size[0] : $size[1];
-      } else {
+      } else if ($size !== true ) {
         $width = $height = $size;
       }
 
       // Dimensions
-      if ($width !== 'null') {
+      if (isset($width) && $width !== 'null') {
         $dimensions .= ' width="'.$width.'"';
       }
 
-      if ($height !== 'null') {
+      if (isset($height) && $height !== 'null') {
         $dimensions .= ' height="'.$height.'"';
       }
 
-      if ($size === true) {
-        $classes .= " svg-".$symbol."-size'";
+      if ($setSize === true ) {
+        $classes .= " svg-".$symbol."-size";
       }
     }
+
+    if (!empty($dimensions)) { $dimensions .= ' '; }
 
     $classes .= "'";
 
     // Final checks
-    if (!$symbolExists || $unsupportedBrowsers && $fallback !== false) {
+    if (!$symbolExists && $fallback === false) {
+      return false;
+    } else if ( $fallback !== false) {
       return '<img '.$classes.$dimensions.' src="'.$fallback.'" alt="'.$symbol.'">';
-    } else if ($symbolExists && $fallback !== false){
+    } else if ($symbolExists){
       return '<svg '.$classes.$dimensions.'><use xlink:href="#'.$symbol.'"></use></svg>';
     }
 
