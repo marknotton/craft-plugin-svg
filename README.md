036dd2e9edf67f13f738d19674162ad321017073
Mark Notton <mark@yellostudio.co.uk>
2017-06-14 15:34:39 +0100

    Removed the over engineered fallback and browser checks to the SVG class. Added option to add classes to SVG elements

--- README.md
+++ README.md
@@ -4,14 +4,14 @@
 
 Implement SVG files and symbols quickly, with browser checks and fallback images.
 
-##Table of Contents
+## Table of Contents
 
 - [SVG](#svg)
 - [Load SVG](#load-svg)
 - [Sprite](#sprite)
 - [Installation](#installation)
 
-##Optional tools to increase functionality
+## Optional tools to increase functionality
 
 - [Browser](https://github.com/marknotton/craft-plugin-browser)
 - [Gulp SVG Sprite](https://github.com/jkphl/gulp-svg-sprite)
@@ -23,47 +23,21 @@ Load in SVG content with fallbacks for specific browsers.
 | # | Parameter         | Type   | Default                       | Optional | Description
 --- | ----------------- | ------ | ----------------------------- | -------- | -----------
 | 1 | SVG file name     | string | null                          | No       | Enter the name of the svg file. You can omit the '.svg' extension, as this will be added automatically.
-| 2 | Fallback image    | string | Same as first parameter       | Yes      | If a fallback image is not passed the svg filename will used. The image directory will be searched, as will the sprites directory, and also various formats will be checked. First match is returned. If a fallback is used, this will be returned in the usual ```<img>``` tag. However, if the fallback is missing, the svg will be used as the source of the ```<img>``` tag instead. You can also use the string 'disable', if you want absolutely no fallbacks.
-| 3 | Browsers to check | string | false                         | Yes      | See [Browser](https://github.com/marknotton/craft-plugin-browser) for documentation. If the browser criteria is a match, a fallback image will be used instead of an SVG... if it exists.
-| 4 | Image directory   | string | environmentVariables > images | Yes      | By default the image location in your general.php Environment Variables will be used by default. However, anything entered in this parameter will overwrite that.
+| 2 | Classes           | string | null                          | Yes      | Add class attributes directly to the SVG element tags
 
 You can change the default image and sprite directories in the SVG plugin settings.
 
-####Example 1:
+#### Example 1:
 Outputs SVG content
 ```
 {{ svg('logo') }}
 ```
 
-####Example 2:
-Outputs SVG content, the fallback image is used for IE9. Otherwise all other browsers will display the SVG as normal.
+#### Example 2:
+Outputs SVG content, with the class "blue"
 ```
-{{ svg('logo', 'logo-dark.png', 'ie 9') }}
-```
+{{ svg('logo', 'blue') }}
 
-####Example 3:
-Outputs SVG content, except for browsers that are IE11 and below where the fallback image is used.
-```
-{{ svg('logo', 'logo-dark.png', 'ie < 11') }}
-```
-
-####Example 4:
-Outputs SVG content, if the svg isn't found... falls back to any image that is named 'logo' with the extension '.png', '.jpg', or '.gif'. First match is returned. This only applies to Firefox browsers in this example.
-```
-{{ svg('logo.svg', false, 'firefox') }}
-```
-
-####Example 5:
-Outputs SVG content, falls back to a standard <img> element using the .svg file. Unless on Edge.
-```
-{{ svg('logo', false, 'edge') }}
-```
-
-####Example 6:
-Outputs SVG content, when 'disable' is passed no fallback will be generated at all.
-```
-{{ svg('logo', 'disable', 'chrome') }}
-```
 
 ----
 ## Load SVG
--- svg/twigextensions/svg.php
+++ svg/twigextensions/svg.php
@@ -1,64 +1,74 @@
 <?php
-namespace Craft;
+			namespace Craft;
 
-use Twig_Extension;
+			use Twig_Extension;
 
-class svg extends \Twig_Extension {
+			class svg extends \Twig_Extension {
 
-  public function getName() {
-    return Craft::t('SVG');
-  }
+			  public function getName() {
+			    return Craft::t('SVG');
+			  }
 
-  public function getFunctions() {
-    return array(
-      'svg' => new \Twig_Function_Method($this, 'svg', array('is_safe' => array('html'))),
-      'loadsvg' => new \Twig_Function_Method($this, 'loadsvg', array('is_safe' => array('html')))
-    );
-  }
+			  public function getFunctions() {
+			    return array(
+			      'svg' => new \Twig_Function_Method($this, 'svg', array('is_safe' => array('html'))),
+			      'loadsvg' => new \Twig_Function_Method($this, 'loadsvg', array('is_safe' => array('html')))
+			    );
+			  }
 
-  public function svg($file, $fallback = null, $browser = null, $imageDir = null) {
+			  public function svg($file, $classes = null) {
 
-    $imageDir = craft()->svg->directory('imagesDirectory','images','/assets/images');
-    $spritesDir = craft()->svg->directory('spritesDirectory','sprites','/assets/images/sprites');
+			    $imageDir = craft()->svg->directory('imagesDirectory','images','/assets/images');
+			    $spritesDir = craft()->svg->directory('spritesDirectory','sprites','/assets/images/sprites');
 
-    $systemPath = craft()->plugins->getPlugin('svg')->getSettings()->relativeLocaleDirectories ? getcwd() : $_SERVER['DOCUMENT_ROOT'];
+			    $systemPath = craft()->plugins->getPlugin('svg')->getSettings()->relativeLocaleDirectories ? getcwd() : $_SERVER['DOCUMENT_ROOT'];
 
-    // Check file extension is svg, otherwise add the '.svg' string.
-    $file = (strlen($file) > 4 && substr($file, -4) == '.svg') ? $file : $file.'.svg';
-    // Checks to see if the file exists
-    if (file_exists($systemPath.$imageDir.$file)) {
-      // If the svg file exits, use this;
-      $fileUrl = $systemPath.$imageDir.$file;
-    } else if (file_exists($systemPath.$spritesDir.$file)) {
-      $fileUrl = $systemPath.$spritesDir.$file;
-    } else {
-      $fileUrl = null;
-    }
+			    // Check file extension is svg, otherwise add the '.svg' string.
+			    $file = (strlen($file) > 4 && substr($file, -4) == '.svg') ? $file : $file.'.svg';
+			    // Checks to see if the file exists
+			    if (file_exists($systemPath.$imageDir.$file)) {
+			      // If the svg file exits, use this;
+			      $fileUrl = $systemPath.$imageDir.$file;
+			    } else if (file_exists($systemPath.$spritesDir.$file)) {
+			      $fileUrl = $systemPath.$spritesDir.$file;
+			    } else {
+			      $fileUrl = null;
+			    }
 
-    $unsupportedBrowsers = $browser ? craft()->svg->browserCheck($browser) : false;
+					if (is_null($fileUrl)) { return $file; }
 
-    // If this browser is supported and the file exists load in all the SVG content directly into the HTML
-    if (!$unsupportedBrowsers && !is_null($fileUrl)) {
-      $content = file_get_contents($fileUrl);
-      $content = str_replace('<?xml version="1.0" encoding="utf-8"?>', '', $content);
-      $content = preg_replace('/<!--(.*)-->/Uis', '', $content);
-      return $content;
-    } else if ( $fallback != 'disable' && $unsupportedBrowsers === true) {
-      // Create an 'id' based on the filename. Slugify and remove extension
-      $id = ElementHelper::createSlug(preg_replace('/.svg$/', '', $file));
+					$svgContent = file_get_contents($fileUrl);
 
-      // Check the fallback is a string, otherwise use the symbol string as the image reference
-      $fallback = is_string($fallback) ? $fallback : $id;
-      $fallback = craft()->svg->fallbackImage($fallback, $imageDir);
+					if ( empty($classes) ) {
+						return $svgContent;
+					} else {
+						$dom = new \DomDocument();
+						$dom->loadXML($svgContent);
 
-      if ($fallback !== false) {
-        // Use the fallback image
-        return "<img id='".$id."' src='".$fallback."' alt='".$id."'>";
-      }
-    }
-  }
+						foreach ($dom->getElementsByTagName('svg') as $element) {
+							if ($element->hasAttribute('class') ) {
 
-  public function loadsvg($file = 'sprite-symbols', $id = null, $imageDir = null) {
-    return $this->svg($file, null, null, $imageDir);
-  }
-}
+								// Array of classes to add
+								$newClasses = explode(" ", $classes);
+
+								// Array of existing classes
+								$existingClasses = explode(" ", $element->getAttribute('class'));
+
+								// Set the class attribute, whilst excluding any duplicates
+								$element->setAttribute('class', rtrim(implode(' ', array_unique(array_merge($newClasses,$existingClasses), SORT_REGULAR))));
+
+							} else {
+
+						    $element->setAttribute('class', $classes);
+							}
+					    return $dom->saveXML();
+					    exit;
+						}
+					}
+
+			  }
+
+			  public function loadsvg($file = 'sprite-symbols', $id = null, $imageDir = null) {
+			    return $this->svg($file, null, null, $imageDir);
+			  }
+			}
